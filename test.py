ec71478eae2be3e04ce7b3e148626ae1631d5a26
moon <shawmakesmagic@gmail.com>
2023-07-16 06:16:23 -0700

    Add inner loop test

--- test.py
+++ test.py
@@ -19,6 +19,28 @@ def step_two(options_dict, loop_data):
     print("step_two: ", options_dict["step_two"])
     return options_dict
 
+def inner_loop(context):
+    loop_data = start(steps=[step_one, step_two], stepped=False)
+    for _ in range(3):
+        step(loop_data)
+        # sleep 1 s
+        time.sleep(0.1)
+    stop(loop_data)  # Stop the loop
+    return context
+
+def test_inner_loop():
+    print("Starting test_start")
+    loop_data = start(steps=[step_one, inner_loop, step_two], stepped=False)
+    for _ in range(3):
+        step(loop_data)
+        # sleep 1 s
+        time.sleep(0.1)
+    assert isinstance(loop_data["thread"], threading.Thread)
+    assert isinstance(loop_data["step_event"], threading.Event)
+    assert loop_data["thread"].is_alive() is True
+    stop(loop_data)  # Stop the loop
+    assert loop_data["thread"].is_alive() is False
+
 
 def test_start():
     print("Starting test_start")

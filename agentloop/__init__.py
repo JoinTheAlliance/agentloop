7cbfe5d1cffe955fdb8a128390c5fea0e9b498ad
moon <shawmakesmagic@gmail.com>
2023-07-18 07:02:51 -0700

    0.1.4

--- agentloop/__init__.py
+++ agentloop/__init__.py
@@ -1,13 +1,3 @@
-"""
-agentloop
-
-A simple, lightweight loop for your agent. Start/stop, step-through, and more.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Autonomous Research Group"
-__credits__ = "https://github.com/AutonomousResearchGroup/agentloop"
-
 from .loop import start, step, stop, loop
 from .input import step_with_input_key
 from .context import create_default_context, create_context_builders
